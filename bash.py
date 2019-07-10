7f4e19c08d9c0efa21fa1968e9503cb83fd5f89b
bengbmccoy <benjamingbmccoy@gmail.com>
2019-07-10 11:20:42 +1000

    the script seems to be working but needs comments and organisation, however I wanted to get a working initial version saved on version control.

--- bash.py
+++ bash.py
@@ -25,16 +25,49 @@ def main():
         freq_dict = dict(count_freq(text))
         freq_df = update_df(freq_df, freq_dict, training_file)
 
-    freq_df = freq_df.fillna(0)
+    # freq_df = freq_df.fillna(0)
     # print(freq_df)
-    print(freq_df.to_string())
+    # print(freq_df.to_string())
 
-    # calc_lr(freq_df)
+    lr_df = calc_lr(freq_df)
+    lr_df = lr_df.fillna(0.0001)
+    # print(lr_df)
+
+    assessment_text = get_words('assessment')
+    assessment_dict = dict(count_freq(assessment_text))
+    # print(assessment_dict)
+
+    assess_inbox_lr = 1
+    assess_spam_lr = 1
+
+    for key, value in assessment_dict.items():
+        try:
+            assess_inbox_lr = assess_inbox_lr * (lr_df.at[key, 'inbox_lr'])
+        except:
+            pass
+        try:
+            assess_spam_lr = assess_spam_lr * (lr_df.at[key, 'spam_lr'])
+        except:
+            pass
+
+    # print(assess_inbox_lr)
+    # print(assess_spam_lr)
+
+    posterior_odds = assess_inbox_lr/assess_spam_lr
+
+    print(posterior_odds)
 
 def calc_lr(freq_df):
 
+    inbox_count = freq_df['inbox_freq'].sum()
+    spam_count = freq_df['spam_freq'].sum()
     for index, row in freq_df.iterrows():
-        pass
+        if row['inbox_freq'] > 0:
+            freq_df.set_value(index, 'inbox_lr', row['inbox_freq']/inbox_count)
+        if row['spam_freq'] > 0:
+            freq_df.set_value(index, 'spam_lr', row['spam_freq']/spam_count)
+
+    return freq_df
 
 def update_df(freq_df, freq_dict, training_file):
 
